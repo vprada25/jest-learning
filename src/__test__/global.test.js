1e7ca9e96ebb06d5b833ae39007ddbc12fc2e84f
vprada25 <v.prada@udla.edu.co>
2022-10-28 16:05:58 -0500

    feat(test): testing the different types of tests according to the structure of the data.

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,6 +1,50 @@
-const text = 'Hello World';
-
+const text = 'Hello World'
+const fruits = ['apple', 'banana', 'orange']
+const reverseString = (str) => str.split('').reverse().join('')
+const reverseString2 = (str) => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error('Error'))
+    }
+    resolve(str.split('').reverse().join(''))
+  })
+}
 
 test('Debe contener un texto', () => {
   expect(text).toMatch(/World/)
 })
+
+test('¿Tiene una banana?', () => {
+  expect(fruits).toContain('banana')
+})
+
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(9)
+})
+
+test('True', () => {
+  expect(true).toBeTruthy()
+})
+
+test('Reverse String', () => {
+  expect(reverseString('Hello')).toBe('olleH')
+})
+
+test('Reverse String 2 resolve', () => {
+  expect(reverseString2('Hello')).resolves.toBe('olleH')
+})
+
+test('Reverse String 2 rechazada', () => {
+  expect(reverseString2()).rejects.toThrowError('Error')
+})
+
+test('Probar async/await', async () => {
+  const string = await reverseString2('Hello')
+  expect(string).toBe('olleH')
+})
+
+afterEach(() => console.log('Despues de cada prueba'))
+afterAll(() => console.log('Despues de todas las pruebas'))
+
+beforeEach(() => console.log('Antes de cada prueba'))
+beforeAll(() => console.log('Antes de todas las pruebas'))
